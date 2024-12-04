7b2fa1190e74c88b66b6d851a8cc70ff66763e76
AlvinBirley <alvinbirley@gmail.com>
2024-12-04 15:04:00 -0600

    Edit newProject to add header and label to newProj divs

--- src/add-project.js
+++ src/add-project.js
@@ -1,11 +1,28 @@
 //**ADD NEW PROJECT TO INNER-PROJECT-DIV**/
 
-//**THIS IS HOW TO GET THE VALUE OF THE TEXT FIELD**/
-const titleValue = document.getElementById("title").value;
 
 //**FUNCTION TO ADD NEW PROJECT DIV TO THE INNER-PROJECT-DIV DIV**/
 export function newProject(){
+
+    //**THIS IS HOW TO GET THE VALUE OF THE TEXT FIELD**/
+const projName = document.getElementById("addProject").value;
+
+    //**SELECT INNER-PROJECT-DIV**/
     const getInnerProjDiv = document.getElementsByClassName("inner-project-div")[0];    
+    
+    //**CREATE NEW PROJECT DIVE AND APPEND TO INNER-PROJECT-DIV**/
     const newProj = document.createElement("div");
+    newProj.classList.add('general');
     getInnerProjDiv.appendChild(newProj);
+
+     //**CREATE HEADER DIV AND APPEND TO newProj DIV**/
+    const projHeader = document.createElement("div");
+    projHeader.classList.add('gen-header');
+    newProj.appendChild(projHeader);
+
+    //**CREATE PROJECT LABEL WITH P TAG AND APPEND TO projHeader DIV**/
+    const makeHeader = document.createElement("p");
+    makeHeader.textContent += projName;
+    projHeader.appendChild(makeHeader);
+
 }
